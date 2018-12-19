a00a9452ccf07d39afdb9d61985c7107b74de26d
Thomas Rush <rush.thomas1@gmail.com>
2018-12-19 14:17:59 -0500

    l

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-fs.mkdir('stuff2', function(){
-  fs.readFile('readMe.txt', 'utf8', function(err, data){
-  fs.writeFile('./stuff2/writeMe.txt', data);  
-  });
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res){
+  res.render('index');
 });
 
-//code
+app.get('/contact',function(req, res){
+  res.render('contact');
+});
+
+app.get('/profile/:name', function(req, res){
+  var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+  res.render('profile', {person: req.params.name, data: data});
+})
+
+app.listen(3000);
