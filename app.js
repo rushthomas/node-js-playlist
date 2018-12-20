7c8c9335659f61a02f022b7009f3891a27af1611
Thomas Rush <rush.thomas1@gmail.com>
2018-12-19 19:19:57 -0500

    a

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -9,7 +12,12 @@ app.get('/', function(req, res){
 });
 
 app.get('/contact',function(req, res){
-  res.render('contact');
+  res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req, res){
+  console.log(req.body);
+  res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', function(req, res){
